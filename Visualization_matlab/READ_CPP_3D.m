d32b9462697b926189795b0320d3f147df8e38db
VicenteB97 <vizenzo97@gmail.com>
2023-07-24 12:28:48 +0200

    Visualization tools

--- Visualization_matlab/READ_CPP_3D.m
+++ Visualization_matlab/READ_CPP_3D.m
@@ -15,11 +15,12 @@ Name_var2 = 'I';
 Name_var3 = 'R';
 
 DIMENSIONS=3;
+conf_lvl = 0.95;
 
 %% CHANGE FOR YOUR CURRENT COMPUTER
 
-Info=readcell  ('../SIMULATION_OUTPUT/Simulation_Info_1.csv');
-Data=readmatrix('../SIMULATION_OUTPUT/Mean_PDFs_1.csv');
+Info=readcell  ('../SIMULATION_OUTPUT/Simulation_Info_5.csv');
+Data=readmatrix('../SIMULATION_OUTPUT/Mean_PDFs_5.csv');
 
 %%
 Total_Pts           = Info{1,1};
@@ -38,14 +39,14 @@ F_Output=zeros(Pts_Per_Dimension, Pts_Per_Dimension, Pts_Per_Dimension);
 
 %%
 [x,y,z] = meshgrid(X,Y,Z);
-xslice = [];    % location of y-z planes
-yslice = [0:.05:1];    % location of x-z plane
-zslice = [0:.05:1];    % location of x-y planes
 
 timesteps = length(Data)/Total_Pts;
-skip = 2;
+skip = 3;
 Integral_vals = zeros(1,floor(timesteps/skip));
 iso_val       = zeros(floor(timesteps/skip),2);
+Marg_X        = zeros(Pts_Per_Dimension,1);
+Marg_Y        = Marg_X;
+Marg_Z        = Marg_X;
 
 for l = 1:skip:timesteps
     fprintf(['At step: ',num2str(l),newline]);
@@ -58,15 +59,28 @@ for l = 1:skip:timesteps
         end
     end
 
+    % COMPUTE MARGINAL DENSITIES
+    Marg_X = h_Y*h_Z*reshape(sum(F_Output,[1 3]),[Pts_Per_Dimension,1]);
+    Marg_Y = h_X*h_Z*reshape(sum(F_Output,[2 3]),[Pts_Per_Dimension,1]);
+    Marg_Z = h_X*h_Y*reshape(sum(F_Output,[1 2]),[Pts_Per_Dimension,1]);
+
     Integral_vals(l)=sum(F_Output,'all')*h_X*h_Y*h_Z;
 
-    iso_val(l,:)=ComputeRegion(Data(1,Pts_Per_Dimension^DIMENSIONS*(l-1)+1:Pts_Per_Dimension^DIMENSIONS*l),0.95*Integral_vals(l),h_X,DIMENSIONS);
+    % COMPUTE THE "HEIGHT" OF THE PDF OVER WHOM WE HAVE "conf_lvl" MASS
+    iso_val(l,:)=ComputeRegion(Data(1,Pts_Per_Dimension^DIMENSIONS*(l-1)+1:Pts_Per_Dimension^DIMENSIONS*l),conf_lvl*Integral_vals(l),h_X,DIMENSIONS);
 
     figure(l)
     xlim([0,1]);xlabel(Name_var1);
     ylim([0,1]);ylabel(Name_var2);
     zlim([0,1]);zlabel(Name_var3);
-%     contourslice(x,y,z,F_Output,xslice,yslice,zslice,10,'linear');view(3);drawnow;colorbar;
     isosurface(x,y,z,F_Output,iso_val(l,1));view(3);colorbar;grid on; grid minor; lightangle(-45,45); drawnow;
+
+    figure(100+l)
+    subplot(3,1,1)
+    plot(X,Marg_X,'r.-');xlabel(Name_var1);ylabel('Probability Density'); drawnow;
+    subplot(3,1,2)
+    plot(Y,Marg_Y,'r.-');xlabel(Name_var2);ylabel('Probability Density'); drawnow;
+    subplot(3,1,3)
+    plot(Z,Marg_Z,'r.-');xlabel(Name_var3);ylabel('Probability Density'); drawnow;
     pause(1);
 end
