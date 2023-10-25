99167b89f4e10b3f1d92cbfc356cd928c014da99
VicenteB97 <vizenzo97gmail.com>
2023-10-25 11:27:24 +0200

    Automatically select GPU with most memory

--- SOURCE_FILES/00_MAIN.cu
+++ SOURCE_FILES/00_MAIN.cu
@@ -31,70 +31,116 @@ void Intro_square_filler(std::string& message, const uint32_t border_length, con
 	message.append("|\n");
 }
 
-int main() {
-	//----------------------------------------------------------------------------------------------------------------------------------------//
+int16_t SetGPU(int16_t& ChosenOne, const uint32_t& border_length){
+
 	int deviceCount = 0;
 	cudaGetDeviceCount(&deviceCount);
+
 	if (deviceCount == 0) {
 		printf("No CUDA-supported GPU found. Exiting program.\n");
 		std::cin.get();
 		return -1;
 	}
-	else {
-		cudaDeviceProp prop;
-		cudaGetDeviceProperties(&prop, 0);
 
-		std::string border 		= "+==========================================================================================+";
-		std::string border_mid 	= "|==========================================================================================|";
-		std::cout << border <<"\n";
-		std::cout << border_mid <<"\n";
-		const uint32_t border_length = border.size();
+	ChosenOne = 0;
 
-		std::string temp = "|	Wecome to the Liouville Eq. simulator. You are using version number "; temp.append(project_version);
+	// If we have more than one, we choose the one with most memory
+	if(deviceCount > 1){
+		
+		std::string temp = "|	Found " ;
+		temp.append(std::to_string(deviceCount));
+		temp.append(" GPUs.");
+		std::cout << temp;
 		Intro_square_filler(temp, border_length, 1, 0);
-		std::cout << temp;
-		std::cout << border_mid <<"\n";
+    	
+		cudaDeviceProp properties;
+		cudaGetDeviceProperties(&properties, ChosenOne);
 
-		temp = "|	Starting simulation using device: "; temp.append(prop.name); temp.append(". Properties:");
-		Intro_square_filler(temp, border_length, 1, 0);
-		std::cout << temp;
+		uint64_t max_memory = properties.totalGlobalMem;
 
-		temp = "|		- Global memory available (GB): "; temp.append(std::to_string(prop.totalGlobalMem/1024/1024/1024));
-		Intro_square_filler(temp, border_length, 2, 1);
-		std::cout << temp;
+		for (uint16_t device = 0; device < deviceCount; device++) {
 
-	#if OUTPUT_INFO
-		temp = "|		- Max. memory bus width: "; temp.append(std::to_string(prop.memoryBusWidth)); temp.append(" bits.");
-		Intro_square_filler(temp, border_length, 2, 1);
-		std::cout << temp;
+			cudaGetDeviceProperties(&properties, device);
+			uint64_t temp_mem = properties.totalGlobalMem;
 
-		temp = "|		- Number of asynchronous engines: " ; temp.append(std::to_string(prop.asyncEngineCount));
-		Intro_square_filler(temp, border_length, 2, 1);
-		std::cout << temp;
+			temp =  "|		- GPU number ";
+			temp.append(std::to_string(device));
+			temp.append(" is: ");
+			temp.append(properties.name);
+			temp.append(" with ");
+			temp.append(std::to_string(temp_mem/1024/1024/1024));
+			temp.append(" GB of global memory.");
+			std::cout << temp;
+			Intro_square_filler(temp, border_length, 2, 1);
 
-		temp = "|		- Number of SMs: "; temp.append(std::to_string(prop.multiProcessorCount));
-		Intro_square_filler(temp, border_length, 2, 1);
-		std::cout << temp;
-
-		temp = "|		- Max. threads per SM: "; temp.append(std::to_string(prop.maxThreadsPerMultiProcessor));
-		Intro_square_filler(temp, border_length, 2, 1);
-		std::cout << temp;
-
-		temp = "|		- Four-byte registers per SM: "; temp.append(std::to_string(prop.regsPerMultiprocessor));
-		Intro_square_filler(temp, border_length, 2, 1);
-		std::cout << temp;
-
-	#endif
-
-		std::cout << border_mid << "\n";
-		std::cout << border << "\n";
-
-		int16_t ret_val = PDF_EVOLUTION(&prop);
-
-		std::cout << "Simulation finished successfully. Press any key to continue...";
-		std::cin.ignore();
-		std::cin.get();
-
-		return ret_val;
+			if (max_memory < temp_mem) {
+				max_memory = temp_mem;
+				ChosenOne = device;
+			}
+		}
 	}
+	return 0;
+}
+
+int main() {
+	//----------------------------------------------------------------------------------------------------------------------------------------//
+
+	std::string border 		= "+==========================================================================================+";
+	std::string border_mid 	= "|==========================================================================================|";
+	std::cout << border <<"\n";
+	std::cout << border_mid <<"\n";
+	const uint32_t border_length = border.size();
+
+	std::string temp = "|	Wecome to the Liouville Eq. simulator. You are using version number "; temp.append(project_version);
+	Intro_square_filler(temp, border_length, 1, 0);
+	std::cout << temp;
+	std::cout << border_mid <<"\n";
+
+	// Choose GPU to use
+	int16_t chosen_GPU, 
+			err_code = SetGPU(chosen_GPU, border_length);
+	if( err_code == -1 ){ return err_code; }
+	
+	cudaDeviceProp prop;
+	cudaGetDeviceProperties(&prop, chosen_GPU);
+
+	temp = "|	Starting simulation using device: "; temp.append(prop.name); temp.append(". Properties:");
+	Intro_square_filler(temp, border_length, 1, 0);
+	std::cout << temp;
+
+	temp = "|		- Global memory available (GB): "; temp.append(std::to_string(prop.totalGlobalMem/1024/1024/1024));
+	Intro_square_filler(temp, border_length, 2, 1);
+	std::cout << temp;
+
+	temp = "|		- Max. memory bus width: "; temp.append(std::to_string(prop.memoryBusWidth)); temp.append(" bits.");
+	Intro_square_filler(temp, border_length, 2, 1);
+	std::cout << temp;
+
+	temp = "|		- Number of asynchronous engines: " ; temp.append(std::to_string(prop.asyncEngineCount));
+	Intro_square_filler(temp, border_length, 2, 1);
+	std::cout << temp;
+
+	temp = "|		- Number of SMs: "; temp.append(std::to_string(prop.multiProcessorCount));
+	Intro_square_filler(temp, border_length, 2, 1);
+	std::cout << temp;
+
+	temp = "|		- Max. threads per SM: "; temp.append(std::to_string(prop.maxThreadsPerMultiProcessor));
+	Intro_square_filler(temp, border_length, 2, 1);
+	std::cout << temp;
+
+	temp = "|		- Four-byte registers per SM: "; temp.append(std::to_string(prop.regsPerMultiprocessor));
+	Intro_square_filler(temp, border_length, 2, 1);
+	std::cout << temp;
+
+	std::cout << border_mid << "\n";
+	std::cout << border << "\n";
+
+	int16_t ret_val = PDF_EVOLUTION(&prop);
+
+	std::cout << "Simulation finished successfully. Press any key to continue...\n";
+	std::cout << border_mid << "\n" << border;
+	std::cin.ignore();
+	std::cin.get();
+
+	return ret_val;
 }
