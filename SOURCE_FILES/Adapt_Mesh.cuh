c1a803603e83b5f056b7e0dfe8d26cf501fbbb0c
VicenteB97 <vizenzo97gmail.com>
2024-01-17 18:23:59 +0100

    Small performance improvements

--- SOURCE_FILES/Adapt_Mesh.cuh
+++ SOURCE_FILES/Adapt_Mesh.cuh
@@ -28,9 +28,9 @@ __device__ inline void _1D_WVLET(TYPE& s1, TYPE& s2){
 /// @param Problem_Domain Problem domain
 /// @param rescaling Rescaling value that indicates the level of the wavelet transform
 /// @return 
-__global__ void D__Wavelet_Transform__F(TYPE*	PDF,
-								  		UINT* nodeIdxs,
-										UINT* isActiveNode,
+__global__ void D__Wavelet_Transform__F(TYPE*		PDF,
+								  		UINT*		nodeIdxs,
+										UINT*		isActiveNode,
 										const Mesh 	BoundingBox,
 										const Mesh	Problem_Domain,
 										const TYPE	rescaling){
@@ -40,36 +40,52 @@ __global__ void D__Wavelet_Transform__F(TYPE*	PDF,
 	// Range guard for out-of-bounds nodes
 	if (globalID >= BoundingBox.Total_Nodes() / powf(rescaling, PHASE_SPACE_DIMENSIONS)) { return; }
 
+	const UINT		totalNodes		= Problem_Domain.Total_Nodes();		// Total nodes in the problem domain
+	const uint16_t	miniSquareNodes = pow(2, PHASE_SPACE_DIMENSIONS);	// Total nodes in each simple wavelet transform (per GPU thread)
+
 	// Global index of the main approximation vertex at the bounding box
 	INT cube_app_IDX = 0;
 
 	// Compute the index and the node per se
-	for (uint16_t d = 0; d < PHASE_SPACE_DIMENSIONS; d++) {
-		INT temp_idx = floor(positive_rem(globalID, pow(BoundingBox.Nodes_per_Dim / rescaling, d + 1)) / pow(BoundingBox.Nodes_per_Dim / rescaling, d)) * rescaling;
 
-		cube_app_IDX += temp_idx * pow(BoundingBox.Nodes_per_Dim, d);
+	TYPE multCounter = 1;	// auxiliary counter: => pow(BoundingBox.Nodes_per_Dim / rescaling, d)
+	TYPE multCounter_2 = 1;	// For the BoundingBox: => pow(BoundingBox.Nodes_per_Dim, d)
+	for (uint16_t d = 0; d < PHASE_SPACE_DIMENSIONS; d++) {
+		INT temp_idx = floor(positive_rem(globalID, multCounter * (BoundingBox.Nodes_per_Dim / rescaling)) / multCounter) * rescaling;
+
+		cube_app_IDX += temp_idx * multCounter_2;
+		multCounter *= BoundingBox.Nodes_per_Dim / rescaling;
+		multCounter_2 *= BoundingBox.Nodes_per_Dim;
 	}
+
+	multCounter = 1;	// Reinitialize for next computations: => pow(2, d)
+	multCounter_2 = 1;	// For the BoundingBox: => pow(BoundingBox.Nodes_per_Dim, d)
 
 	// 1 set of wavelets per dimension (1D: horizontal; 2D: Horizontal + Vertical; 3D: Horz + Vert + Deep; ...)
 	for (uint16_t d = 0; d < PHASE_SPACE_DIMENSIONS; d++) {
 
 		// Go through all the vertices that are defined by the main cube approximation vertex
-		for (INT k = 0; k < pow(2, PHASE_SPACE_DIMENSIONS); k++) {
+		for (INT k = 0; k < miniSquareNodes; k++) {
 
 			// If we are at the current approximation vertex:
-			if (floor(positive_rem(k, pow(2, d + 1)) / pow(2, d)) == 0) {
+			if (floor(positive_rem(k, 2 * multCounter) / multCounter) == 0) {		// here, multCounter == pow(2, d)
 
 				// Copmute approximation node
 				INT app_IDX_at_BBox = cube_app_IDX;
 
-				for (uint16_t j = 0; j < PHASE_SPACE_DIMENSIONS; j++) {
-					INT temp = floorf(positive_rem(k, pow(2, j + 1)) / pow(2, j)) * rescaling / 2;	// j-th component index
+				UINT multCounter_3 = 1;	// => pow(2, j)
+				UINT multCounter_4 = 1;	// => pow(BoundingBox.Nodes_per_Dim, j)
 
-					app_IDX_at_BBox += temp * pow(BoundingBox.Nodes_per_Dim, j);
+				for (uint16_t j = 0; j < PHASE_SPACE_DIMENSIONS; j++) {
+					INT temp = floorf(positive_rem(k, multCounter_3 * 2) / multCounter_3) * rescaling / 2;	// j-th component index
+
+					app_IDX_at_BBox += temp * multCounter_4;
+					multCounter_3	*= 2;
+					multCounter_4	*= BoundingBox.Nodes_per_Dim;
 				}
 
 				// Compute corresponding detail node
-				INT det_IDX_at_BBox = app_IDX_at_BBox + pow(BoundingBox.Nodes_per_Dim, d) * rescaling / 2;
+				INT det_IDX_at_BBox = app_IDX_at_BBox + multCounter_2 * rescaling / 2;
 
 				Particle app_node = BoundingBox.Get_node(app_IDX_at_BBox);
 				Particle det_node = BoundingBox.Get_node(det_IDX_at_BBox);
@@ -85,20 +101,25 @@ __global__ void D__Wavelet_Transform__F(TYPE*	PDF,
 				}
 			}
 		}
+		multCounter		*= 2;
+		multCounter_2	*= BoundingBox.Nodes_per_Dim;
 	}
 
 	// Now we have to go see what happens with the outputs
 	nodeIdxs[cube_app_IDX] = 0;
 
-	for (UINT k = 1; k < pow(2, PHASE_SPACE_DIMENSIONS); k++) {
+	for (UINT k = 1; k < miniSquareNodes; k++) {
 
 		Particle visit_node = BoundingBox.Get_node(cube_app_IDX);
 
+		multCounter = 1;	// restart the counter
+
 		// Get the indeces at the bounding box:
 		for (uint16_t d = 0; d < PHASE_SPACE_DIMENSIONS; d++) {
-			INT temp = floorf(positive_rem(k, pow(2, d + 1)) / pow(2, d)) * rescaling / 2;	// j-th component index
+			INT temp = floorf(positive_rem(k, multCounter * 2) / multCounter) * rescaling / 2;	// j-th component index
 
 			visit_node.dim[d] += temp * BoundingBox.Discr_length();
+			multCounter *= 2;
 		}
 
 		if (Problem_Domain.Contains_particle(visit_node)) {
@@ -159,10 +180,13 @@ int16_t setInitialParticles(const thrust::host_vector<TYPE>&	H_PDF,
 	Supp_BBox.Squarify();	// Make it square
 	Supp_BBox.Nodes_per_Dim = (Supp_BBox.Boundary_sup.dim[0] - Supp_BBox.Boundary_inf.dim[0]) / Problem_Domain.Discr_length() + 1;
 
-	const double refinementLvl = log2(Supp_BBox.Nodes_per_Dim);
+	double refinementLvl = log2(Supp_BBox.Nodes_per_Dim);
 
 	if (fmod(refinementLvl, 1) != 0) {
 		Supp_BBox.Nodes_per_Dim = pow(2, ceil(refinementLvl));
+
+		// Rewrite the refinement level value
+		refinementLvl = log2(Supp_BBox.Nodes_per_Dim);
 
 		if (Supp_BBox.Nodes_per_Dim >= Problem_Domain.Nodes_per_Dim) {Supp_BBox = Problem_Domain;}
 		else{
@@ -178,7 +202,7 @@ int16_t setInitialParticles(const thrust::host_vector<TYPE>&	H_PDF,
 	thrust::device_vector<UINT> nodeIdxs(Supp_BBox.Total_Nodes(), 0);
 	thrust::device_vector<UINT> isAssignedNode(Supp_BBox.Total_Nodes(), 0);
 
-	for (uint16_t k = 0; k < log2(Supp_BBox.Nodes_per_Dim); k++) {
+	for (uint16_t k = 0; k < refinementLvl; k++) {
 
 		uint16_t Threads = fmin(THREADS_P_BLK, Supp_BBox.Total_Nodes()/pow(rescaling,PHASE_SPACE_DIMENSIONS) );
 		UINT	 Blocks	 = floor((Supp_BBox.Total_Nodes()/pow(rescaling, PHASE_SPACE_DIMENSIONS) - 1) / Threads) + 1;
