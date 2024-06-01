d9635ddda34d8ef8a54b4e5ef82e85dd5f3a7c53
VicenteB97 <vizenzo97gmail.com>
2024-06-01 18:53:44 +0200

    Backup

--- Visualization_matlab/ComputeRegion.m
+++ Visualization_matlab/ComputeRegion.m
@@ -3,7 +3,7 @@ if confidenceLvl==1
     FinalRegion_bound=1e-9;
     Int_output = 1;
 else
-    
+
     f_Disc = reshape(PDF.',1,[]);
 
     % First, we sort the values vector (using the GPU...much faster):
@@ -11,39 +11,39 @@ else
     f_disc_gpu=sort(f_disc_gpu,'descend');
     f_Disc=gather(f_disc_gpu);
 
-%     f_Disc=sort(f_Disc,'descend');
-    
-%     tol=0.01;
+    %     f_Disc=sort(f_Disc,'descend');
+
+    %     tol=0.01;
     tol=0.005;
 
     % We are given the points as vectors, we are going to try and compute the
     % condidence region as a level set
     IntegralA=1;
     LambdaA=0;
-    
+
     IntegralB=0;
     LambdaB=f_Disc(1,1); % What is the max value of the function?
     i_new=1;
     i_new2=1;
-    
+
     error=1;
-    
+
     maxIts=0;
-    
+
     while (1) % to make sure there is no overflow due to slow convergence
-        
+
         LambdaC=0.5*(LambdaA+LambdaB);
-        
+
         for i=i_new:length(f_Disc)
             if f_Disc(i)>=LambdaC
                 i_new2=i;
             end
         end
-    
+
         IntegralC=IntegralB+h^PHASE_SPACE_DIMENSIONS*sum(f_disc_gpu(i_new:i_new2));
-    
+
         error=IntegralC-confidenceLvl;
-    
+
         if maxIts >= 10000
             FinalRegion_bound=1e-9;
             Int_output = 1;
--- Visualization_matlab/READ_CPP_3D.m
+++ Visualization_matlab/READ_CPP_3D.m
@@ -1,7 +1,7 @@
 clear
 close all
 clc
-
+set(0,'defaulttextinterpreter','latex')
 delete(gcp('nocreate'));
 
 %% PRE-(POST-PROCESSING)
@@ -20,9 +20,17 @@ Name_var3 = 'R';
 PHASE_SPACE_DIMENSIONS=3;
 conf_lvl = 0.95;
 
-%% CHANGE FOR YOUR CURRENT COMPUTER
 
-Info=readcell  ('../output/Simulation_Info_0.csv');
+%% CHANGE FOR YOUR CURRENT COMPUTER
+% best in S: 4.7
+% best overall 5.3
+% Info=readcell  ('../output/SIR_r_5y3_reinit_2_samples_32_Simulation_info_0.csv');
+% fileID = fopen('../output/SIR_r_5y3_reinit_2_samples_32_Mean_PDFs_0.bin');
+
+Info=readcell  ('../output/SIR_r_6y3_reinit_2_samples_8_high_Simulation_info_0.csv');
+fileID = fopen('../output/SIR_r_6y3_reinit_2_samples_8_high_Mean_PDFs_0.bin');
+
+
 
 Total_Pts           = Info{1,1};
 Pts_Per_Dimension   = Info{1,2};
@@ -30,17 +38,17 @@ Pts_Per_Dimension   = Info{1,2};
 h_X =(Info{1,4}-Info{1,3})/(Pts_Per_Dimension-1);
 X   =Info{1,3}:h_X:Info{1,4};
 
-h_Y =(Info{1,6}-Info{1,5})/(Pts_Per_Dimension-1); 
+h_Y =(Info{1,6}-Info{1,5})/(Pts_Per_Dimension-1);
 Y   =Info{1,5}:h_X:Info{1,6};
 
-h_Z =(Info{1,8}-Info{1,7})/(Pts_Per_Dimension-1); 
+h_Z =(Info{1,8}-Info{1,7})/(Pts_Per_Dimension-1);
 Z   =Info{1,7}:h_X:Info{1,8};
 
 % F_Output=zeros(Pts_Per_Dimension, Pts_Per_Dimension, Pts_Per_Dimension, timesteps);
 timesteps = length(Info);
 
 %%
-fileID = fopen('../output/Mean_PDFs_0.bin');
+
 Data=fread(fileID,[Pts_Per_Dimension^3,timesteps],'float');
 fclose(fileID);
 
@@ -61,7 +69,10 @@ Marg_X        = zeros(Pts_Per_Dimension,1);
 Marg_Y        = Marg_X;
 Marg_Z        = Marg_X;
 
-for l = 1:skip:timesteps
+ttss = ceil(8/30*(timesteps))
+output_name = ['../output/liouville_SIR_r_6y3_reinit_2_samples_32_time_', num2str(ttss-1), '.pdf'];
+output_name_ci = ['../output/liouville_SIR_ci_r_6y3_reinit_2_samples_32_time_', num2str(ttss-1), '.pdf'];
+for l = ttss:ttss
     fprintf(['At step: ',num2str(l),newline]);
 
     % COMPUTE MARGINAL DENSITIES
@@ -74,20 +85,28 @@ for l = 1:skip:timesteps
     % COMPUTE THE "HEIGHT" OF THE PDF OVER WHOM WE HAVE "conf_lvl" MASS
     iso_val(l,:)=ComputeRegion(Data(:,l)',conf_lvl*Integral_vals(l),h_X,PHASE_SPACE_DIMENSIONS);
 
-    figure(l)
+    figure(l);
     xlim([0,1]);xlabel(Name_var2);
     ylim([0,1]);ylabel(Name_var1);
     zlim([0,1]);zlabel(Name_var3);
-    title(['Confidence surface at time ', num2str(Info{2,l})]);
-    isosurface(x,y,z,F_Output(:,:,:,l),iso_val(l,1));view(3);colorbar;grid on; grid minor; lightangle(-45,45); drawnow;
+    title(['L: SIR confidence surface at $t = ', num2str(Info{2,l}), '$'], 'FontSize', 16);
+    isosurface(x,y,z,F_Output(:,:,:,l),iso_val(l,1));view(3);colorbar;
+    grid on; grid minor; lightangle(-45,45); drawnow;
+    exportgraphics(gcf,output_name_ci,"Resolution",300,'ContentType','vector',...
+        'BackgroundColor','none')
 
-    figure(100+l)
-    subplot(3,1,1)
-    plot(X,Marg_X,'r.-');xlabel(Name_var1);ylabel('Probability Density'); drawnow;
-    subplot(3,1,2)
-    plot(Y,Marg_Y,'r.-');xlabel(Name_var2);ylabel('Probability Density'); drawnow;
-    subplot(3,1,3)
-    plot(Z,Marg_Z,'r.-');xlabel(Name_var3);ylabel('Probability Density'); drawnow;
-    sgtitle(['Marginal PDFs at time ', num2str(Info{2,l})]);
-    pause(0.5);
+    figure(2);
+    hold on;
+    grid on;
+    xlim([0,1]);
+     ylim([0,35]);
+    %     subplot(3,1,1)
+    plot(X,Marg_X,'r-');
+    plot(Y,Marg_Y,'g--');
+    plot(Z,Marg_Z,'b.-');
+    legend("S","I","R")
+    title( ['L: SIR per-component PDFs at $t =  ', num2str(Info{2,l}), '$'],'FontSize', 16);
+    hold off;
+    exportgraphics(gcf,output_name,"Resolution",300,'ContentType','vector',...
+        'BackgroundColor','none')
 end
